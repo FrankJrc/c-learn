66ed41a402703f9bcc6cabbc82ff3f766540aea0
Rongchen <dit.frank.jia@gmail.com>
2019-05-18 12:14:18 +0100

    alog -- binary search

--- algo/src/binary_search_demo.cpp
+++ algo/src/binary_search_demo.cpp
@@ -9,7 +9,30 @@ binary_search_demo::~binary_search_demo()
 {
     //dtor
 }
-
+template<typename TIter, typename T>
+bool my_binary_search(TIter left, TIter right, const T& target)
+{
+    bool found = false;
+    while(left<right)
+    {
+        auto mid = left;
+        std::advance(mid, (right-left)/2);
+        if(*mid == target)
+        {
+            found = true;
+            break;
+        }
+        else if(*mid < target)
+        {
+            left = mid+1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return found;
+}
 void binary_search_demo::run()
 {
     std::cout << "binary demo start" << std::endl;
@@ -21,25 +44,22 @@ void binary_search_demo::run()
     {
         auto left = odds.begin();
         auto right = odds.end();
-        bool found = false;
-        while(left<right)
-        {
-            auto mid = left;
-            std::advance(mid, (right-left)/2);
-            if(*mid == target)
-            {
-                found = true;
-                break;
-            }
-            else if(*mid < target)
-            {
-                left = mid+1;
-            }
-            else
-            {
-                right = mid;
-            }
-        }
+        auto found = my_binary_search(left, right, target);
+        if(found)
+            std::cout << target << " ok" << std::endl;
+        else
+            std::cout << target << " fail" << std::endl;
+    }
+
+
+    std::vector<int> even{1,2,3,4};
+    std::for_each(even.begin(), even.end(), [](const int& i){std::cout << "vector element is " << i << std::endl;});
+
+    for(int target=0; target<=6; ++target)
+    {
+        auto left = even.begin();
+        auto right = even.end();
+        auto found = my_binary_search(left, right, target);
         if(found)
             std::cout << target << " ok" << std::endl;
         else
